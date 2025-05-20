76624579440e71cb280f3ee177372828b6a9f0e7
martinamato <martiin.amato@gmail.com>
2025-05-20 16:14:36 -0300

    Update name NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
+import Navbar from './components/NavBar';
 import Dashboard from './pages/Dashboard';
 import Expenses from './pages/Expenses';
 import Optimizer from './pages/Optimizer';
